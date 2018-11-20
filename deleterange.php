5175bf53d7c7dc937bb500488d8a038e05b7377a
Christophe Delaigue <christophe.delaigue.pro@gmail.com>
2018-11-20 16:50:02 +0100

    Create deleterange.php

---
+++ deleterange.php
@@ -0,0 +1,38 @@
+<?php
+function getIpRange(  $cidr) {
+
+    list($ip, $mask) = explode('/', $cidr);
+
+    $maskBinStr =str_repeat("1", $mask ) . str_repeat("0", 32-$mask );      //net mask binary string
+    $inverseMaskBinStr = str_repeat("0", $mask ) . str_repeat("1",  32-$mask ); //inverse mask
+
+    $ipLong = ip2long( $ip );
+    $ipMaskLong = bindec( $maskBinStr );
+    $inverseIpMaskLong = bindec( $inverseMaskBinStr );
+    $netWork = $ipLong & $ipMaskLong;
+
+    $start = $netWork+1;//ignore network ID(eg: 192.168.1.0)
+
+    $end = ($netWork | $inverseIpMaskLong) -1 ; //ignore brocast IP(eg: 192.168.1.255)
+    return array('firstIP' => $start, 'lastIP' => $end );
+}
+
+function getEachIpInRange ( $cidr) {
+    $ips = array();
+    $range = getIpRange($cidr);
+    for ($ip = $range['firstIP']; $ip <= $range['lastIP']; $ip++) {
+        $ips[] = long2ip($ip);
+    }
+    return $ips;
+}
+
+$cidr = '10.112.0.0/16'; // max. 30 ips
+$ips = getEachIpInRange ( $cidr);
+foreach($ips as $ip)
+{
+        echo $ip."\n";
+        system("./delhost.php ".$ip);
+}
+
+
+?>
